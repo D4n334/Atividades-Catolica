a18e942c669ddc942fc972e638e79b65860921f6
Daniel Alexandre Boaventura <88729154+D4n334@users.noreply.github.com>
2024-04-10 23:49:12 -0300

    Add files via upload

---
+++ Atividade 1/VariaveisOperadores.js
@@ -0,0 +1,21 @@
+// Declarar o primeiro número
+const numA = 10;
+
+// Declarar o segundo número
+const numB = 5;
+
+// Adição
+const resultadoSoma = numA + numB;
+console.log(`Resultado da soma entre ${numA} e ${numB} é ${resultadoSoma}`);
+
+// Subtração
+const resultadoSubtracao = numA - numB;
+console.log(`Resultado da subtração entre ${numA} e ${numB} é ${resultadoSubtracao}`);
+
+// Multiplicação
+const resultadoMultiplicacao = numA * numB;
+console.log(`Resultado da multiplicação entre ${numA} e ${numB} é ${resultadoMultiplicacao}`);
+
+// Divisão
+const resultadoDivisao = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB} é ${resultadoDivisao}`);
