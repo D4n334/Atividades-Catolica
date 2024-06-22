9093428a0ea2c8473bcd43ef0d57e553c75abef8
Daniel Alexandre Boaventura <88729154+D4n334@users.noreply.github.com>
2024-06-22 17:28:20 -0300

    Delete Olha_a_Pedra/server directory

--- Olha_a_Pedra/server/models/Ranking.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const RankingSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    score: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('Ranking', RankingSchema);
